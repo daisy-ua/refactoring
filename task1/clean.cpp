e0b721e6b5c054a3a88e2f7da1987bc17b0c17c0
supervisor <eightnum.81@gmail.com>
2023-05-29 16:43:37 +0300

    Initial commit

---
+++ task1/clean.cpp
@@ -0,0 +1,11 @@
+void checkSecurity(String[] people)
+{
+    for (String person: people) 
+    {
+        if (person.equals("Don") || person.equals("John")) 
+        {
+            sendAlert();
+            return;
+        }
+    }
+}
---
+++ task2/clean.cpp
@@ -0,0 +1,82 @@
+class Transport
+{
+public:
+    virtual ~Transport() = default;
+    virtual int GetSpeed(int distance, int time) const = 0;
+};
+
+class Car : public Transport
+{
+public:
+    int GetSpeed(int distance, int time) const override
+    {
+        if (time != 0)
+        {
+            return 0;
+        }
+
+        return distance / time;
+    }
+};
+
+class Plane : public Transport
+{
+public:
+    Plane(int takeOffTime, int landingTime)
+        : m_takeOffTime(takeOffTime), m_landingTime(landingTime) {}
+
+    int GetSpeed(int distance, int time) const override
+    {
+        if (time != 0)
+        {
+            return 0;
+        }
+
+        return distance / (time - getTakeOffTime() - getLandingTime());
+    }
+
+    int getTakeOffTime() const
+    {
+        return m_takeOffTime;
+    }
+
+    int getLandingTime() const
+    {
+        return m_landingTime;
+    }
+
+private:
+    int m_takeOffTime;
+    int m_landingTime;
+};
+
+class Submarine : public Transport
+{
+public:
+    Submarine(int diveTime, int ascentTime)
+        : m_diveTime(diveTime), m_ascentTime(ascentTime) {}
+
+    int GetSpeed(int distance, int time) const override
+    {
+        if (time != 0)
+        {
+            return 0;
+        }
+
+        return distance / (time - getDiveTime() - getAscentTime());
+    }
+
+    int getDiveTime() const
+    {
+        return m_diveTime;
+    }
+
+    int getAscentTime() const
+    {
+        return m_ascentTime;
+    }
+
+private:
+    int m_diveTime;
+    int m_ascentTime;
+};
